3b855adf2a7afd8c03372b7c782ccf7c29f4456e
NelsonMALGAS <zmalgas69@gmail.com>
2023-03-28 14:54:35 +0200

    Modified Index.html & added script2.js

---
+++ Javascript/script2.js
@@ -0,0 +1,35 @@
+// This is the first name  of the user
+//CHALLENGE 1
+const user='John';
+
+/**
+ * This is the last name of the user
+ */
+const surname='Smith';
+
+console.log(user , surname);
+// (c) ACME Inc. 2010 */
+
+/**
+ * This is the date when  the user created their account
+ */
+
+const date='10/07/2014';
+
+console.log(date);
+
+
+/*
+It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors
+*/
+
+console.warn("Security scan Starting");
+
+//It is important to let user know when they can close the page
+
+console.info("Please wait for scan to complete before closing the browser.")
+
+
