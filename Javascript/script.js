f4d32fb3fbfc6a0546e1c45ff54840f8f0789036
NelsonMALGAS <zmalgas69@gmail.com>
2023-03-28 09:18:29 +0200

    modified script.js file

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,7 +1,7 @@
 const number=document.querySelector('[data-key="number"]')
 const subtract=document.querySelector('[data-key="subtract"]')
 const add=document.querySelector('[data-key="add"]')
-const MIN_NUMBER=0
+
 
 const addHandler=()=>{
     const newValue=parseInt(number.value)+1
