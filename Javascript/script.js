3701bacf9e7213ff304b9d53fff40eabccec0d69
NelsonMALGAS <zmalgas69@gmail.com>
2023-04-22 13:05:06 +0200

    Added space to variables

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,5 +1,6 @@
 const MAX_NUMBER =15;
 const MIN_NUMBER = -5;
+
 const number=document.querySelector('[data-key="number"]')
 const subtract=document.querySelector('[data-key="subtract"]')
 const add=document.querySelector('[data-key="add"]')
