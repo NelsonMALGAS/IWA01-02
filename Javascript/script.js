d82b18a6f2d09d4c171b32be34a1b4cb40c21552
NelsonMALGAS <zmalgas69@gmail.com>
2023-04-22 13:03:59 +0200

    Modified js file

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,5 @@
-
+const MAX_NUMBER =15;
+const MIN_NUMBER = -5;
 const number=document.querySelector('[data-key="number"]')
 const subtract=document.querySelector('[data-key="subtract"]')
 const add=document.querySelector('[data-key="add"]')
@@ -7,11 +8,26 @@ const add=document.querySelector('[data-key="add"]')
 const addHandler=()=>{
     const newValue=parseInt(number.value)+1
     number.value = newValue;
+
+    
+    if(newValue >=MAX_NUMBER){
+        (add.disabled=true)
+    }else{
+        (add.disabled=false)
+
+    }
    
 }
 const subtractHandler=()=>{
     const newValue=parseInt(number.value)-1
     number.value = newValue;
+
+    if(add.disabled ===true){
+        add.disabled = false
+    }
+    if(newValue <= MIN_NUMBER){
+        subtract.disabled = true
+    }
     
 }
 
